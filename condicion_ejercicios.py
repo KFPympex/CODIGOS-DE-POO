78af78150c85ab2c7c5661056517527bbfaa2ebc
Kevin Lira <160613820+KFPympex@users.noreply.github.com>
2024-02-22 07:29:23 -0600

    Create condicion_ejercicios.py

---
+++ condicion_ejercicios.py
@@ -0,0 +1,22 @@
+cadena = "Hola mundo python"
+
+for c in cadena:
+    print (c)
+
+
+print("\n" * 2)#
+
+#a, e, i, o , u
+
+for caracter in cadena:
+
+    print(caracter)
+
+
+print("\n" * 2)#
+
+cadena = "Hola mundo python"
+vocales = ("a", "e", "i", "o", "u")
+for caracter in cadena:
+
+    print()
