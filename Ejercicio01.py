7728627055da52d649fca42f3c9cc3d657277919
Kevin Lira <160613820+KFPympex@users.noreply.github.com>
2024-02-21 18:37:18 -0600

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,12 @@
+"""
+   Nombre: Kevin Francisco Lira Barrera
+   Matricula: 1723110397
+   Grupo:TI21
+   Fecha: 18/01/2023
+   Descripcion: Programa que define a un objeto
+"""
+class A:#define una clase de nombre A
+    matricula=None#declara la variable matricula
+    nombre=None#declara la variable nombre
+objetoA=A()#genera una instancia de la clase A
+print (objetoA.nombre)#imprime el valor de nombre
